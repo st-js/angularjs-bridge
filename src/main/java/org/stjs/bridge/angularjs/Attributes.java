78273c74e1f93a9bf76879ddb1be2ccc73328f33
Alexandru Craciun <ax.craciun@gmail.com>
2014-01-02 12:45:24 +0100

    jquery->1.9.1.bv1

--- src/main/java/org/stjs/bridge/angularjs/Attributes.java
+++ src/main/java/org/stjs/bridge/angularjs/Attributes.java
@@ -1,10 +1,24 @@
 package org.stjs.bridge.angularjs;
 
+import java.util.Iterator;
+
 import org.stjs.javascript.Map;
+import org.stjs.javascript.annotation.Template;
 import org.stjs.javascript.functions.Callback1;
 
-public abstract class Attributes implements Map<String, String> {
+public abstract class Attributes implements Iterable<String> {
 	public Map<String, String> $attr;
+
+	public native Iterator<String> iterator();
+
+	@Template("get")
+	public native String $get(String key);
+
+	@Template("put")
+	public native void $put(String key, String value);
+
+	@Template("delete")
+	public native void $delete(String key);
 
 	public native void $addClass(String classVal);
 
